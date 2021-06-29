5cfb902bad87283888a49d826c14b41b71fea354
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2021-06-29 11:43:08 -0500

    Versión prefinal
    Se corrigieron los valores de los números de 8 bit, así como se agregó
    un archivo de restricciones. A este último le falta definir los 8 bits
    de entrada y el 1 bit de salida del PWM

--- PWM.vhd
+++ PWM.vhd
@@ -14,45 +14,8 @@ architecture BLOCK_PWM_ARCH of BLOCK_PWM is
     signal counterLow: integer := 15;
     signal tempCounterLow: integer := 1;
 begin
-    process(clk, counterLow, in_PWM)
+    process(clk, in_PWM)
     begin
-        case in_PWM is
-            when "00000000" =>
-                counterLow <= 15;
-            when "00000001" =>
-                counterLow <= 16;
-            when "00000010" =>
-                counterLow <= 17;
-            when "00000011" =>
-                counterLow <= 18;
-            when "00000100" =>
-                counterLow <= 19;
-            when "00000101" =>
-                counterLow <= 20;
-            when "00000110" =>
-                counterLow <= 21;
-            when "00000111" =>
-                counterLow <= 22;
-            when "00001000" =>
-                counterLow <= 23;
-            when "00001001" =>
-                counterLow <= 24;
-            when "00001010" =>
-                counterLow <= 25;
-            when "00001011" =>
-                counterLow <= 26;
-            when "00001100" =>
-                counterLow <= 27;
-            when "00001101" =>
-                counterLow <= 28;
-            when "00001110" =>
-                counterLow <= 29;
-            when "00001111" =>
-                counterLow <= 30;
-            when others =>
-                counterLow <= 15;
-        end case;
-
         if rising_edge(clk) then
             tempCounterLow <= tempCounterLow + 1;
             if tempCounterLow = counterLow then
@@ -62,6 +25,43 @@ begin
                 tempCounterLow <= 1;
                 counterLow <= 15;
             end if;
+
+            case in_PWM is
+                when "00000000" =>
+                    counterLow <= 15;
+                when "00010100" =>
+                    counterLow <= 16;
+                when "00011110" =>
+                    counterLow <= 17;
+                when "00101000" =>
+                    counterLow <= 18;
+                when "00110010" =>
+                    counterLow <= 19;
+                when "00111100" =>
+                    counterLow <= 20;
+                when "01000110" =>
+                    counterLow <= 21;
+                when "01010000" =>
+                    counterLow <= 22;
+                when "01011010" =>
+                    counterLow <= 23;
+                when "01100100" =>
+                    counterLow <= 24;
+                when "01101110" =>
+                    counterLow <= 25;
+                when "01111000" =>
+                    counterLow <= 26;
+                when "10000010" =>
+                    counterLow <= 27;
+                when "10001100" =>
+                    counterLow <= 28;
+                when "10010110" =>
+                    counterLow <= 29;
+                when "10100000" =>
+                    counterLow <= 30;
+                when others =>
+                    counterLow <= 15;
+            end case;
         end if;
     end process;
     out_PWM <= tempOut;
